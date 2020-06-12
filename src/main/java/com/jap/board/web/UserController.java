c6bb7c087f2a0b4bfffbacffae73a84416eed2eb
tychejin1218 <tychejin@naver.com>
2020-06-12 16:05:00 +0900

    자바 객체와 테이블 매핑, 회원가입 기능 구현  - JPA 설정

--- src/main/java/com/jap/board/web/UserController.java
+++ src/main/java/com/jap/board/web/UserController.java
@@ -1,37 +1,30 @@
 package com.jap.board.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.jap.board.domain.User;
+import com.jap.board.domain.UserRepository;
+
 @Controller
 public class UserController {
 
-	private List<User> users = new ArrayList<User>();
-	
-	/*@PostMapping("/create")
-	public String create(String userId, String password, String name, String email) {
-		System.out.println("userId : " + userId);
-		System.out.println("password : " + password);
-		System.out.println("name : " + name);
-		System.out.println("email : " + email);
-		return "index";
-	}*/
+	@Autowired
+	private UserRepository userRepository;
 
 	@PostMapping("/create")
 	public String create(User user) {
 		System.out.println("User : " + user);
-		users.add(user);
+		userRepository.save(user);
 		return "redirect:/list";
 	}
-	
+
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		return "list";
 	}
 }
