5583df118f7d3bb05d37c7a4a19720801400274e
tychejin1218 <tychejin@naver.com>
2020-06-15 18:11:02 +0900

    질문하기, 질문목록 기능 구현

---
+++ src/main/java/com/jap/board/domain/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.jap.board.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+}
---
+++ src/main/java/com/jap/board/web/QuestionController.java
@@ -0,0 +1,47 @@
+package com.jap.board.web;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jap.board.domain.Question;
+import com.jap.board.domain.QuestionRepository;
+import com.jap.board.domain.User;
+
+@Controller
+@RequestMapping("/questions")
+public class QuestionController {
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@GetMapping("/form")
+	public String form(HttpSession session) {
+
+		if (!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+
+		return "/qna/form";
+	}
+
+	@PostMapping("")
+	public String create(HttpSession session, String title, String contents) {
+
+		if (!HttpSessionUtils.isLoginUser(session)) {
+			return "/users/loginForm";
+		}
+
+		User sessionUser = HttpSessionUtils.getUserFromSession(session);
+		Question question = new Question(sessionUser.getUserId(), title, contents);
+		
+		System.out.println("question : " + question);
+		questionRepository.save(question);
+		
+		return "redirect:/";
+	}
+}
