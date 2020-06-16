38904c9abbf58f82d91a2f6ce4590b69960aa073
tychejin1218 <tychejin@naver.com>
2020-06-16 14:45:49 +0900

    질문 상세보기 기능 구현

--- src/main/java/com/jap/board/web/QuestionController.java
+++ src/main/java/com/jap/board/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,7 +17,7 @@ import com.jap.board.domain.User;
 @Controller
 @RequestMapping("/questions")
 public class QuestionController {
-	
+
 	@Autowired
 	private QuestionRepository questionRepository;
 
@@ -38,10 +40,18 @@ public class QuestionController {
 
 		User sessionUser = HttpSessionUtils.getUserFromSession(session);
 		Question question = new Question(sessionUser, title, contents);
-		
+
 		System.out.println("question : " + question);
 		questionRepository.save(question);
-		
+
 		return "redirect:/";
+	}
+
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+
+		model.addAttribute("question", questionRepository.findById(id)
+		                                                 .get());
+		return "/qna/show";
 	}
 }
