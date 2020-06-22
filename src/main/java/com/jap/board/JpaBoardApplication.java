ccf9094c7fe15ecdc275f8824d11cf92a89fc85c
tychejin1218 <tychejin@naver.com>
2020-06-22 16:25:25 +0900

    Swagger 라이브러리를 통한 API 문서화 및 테스트

--- src/main/java/com/jap/board/JpaBoardApplication.java
+++ src/main/java/com/jap/board/JpaBoardApplication.java
@@ -6,8 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-@EnableJpaAuditing
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableSwagger2
 public class JpaBoardApplication {
 
 	public static void main(String[] args) {
@@ -15,11 +23,32 @@ public class JpaBoardApplication {
 	}
 
 	/**
-	 * HiddenHttpMethodFilter는 요청 파라미터에 "_method"가 있을 경우 파라미터의 value을 요청 방식으로 사용하도록 스프링 MVC의 관련 정보를 설정하는 역활
+	 * HiddenHttpMethodFilter는 요청 파라미터에 "_method"가 있을 경우 파라미터의 value을 요청 방식으로
+	 * 사용하도록 스프링 MVC의 관련 정보를 설정하는 역활
 	 */
 	@Bean
-	public HiddenHttpMethodFilter hiddenHttpMethodFilter() { 
+	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 		HiddenHttpMethodFilter hiddenHttpMethodFilter = new HiddenHttpMethodFilter();
 		return hiddenHttpMethodFilter;
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("JPA_BOARD")
+		                                              .apiInfo(apiInfo())
+		                                              .select()
+		                                              .paths(PathSelectors.ant("/api/**"))
+		                                              .build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("JPA_BOARD API")
+		                           .description("JPA_BOARD API")
+		                           .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+		                           .contact("Niklas Heidloff")
+		                           .license("Apache License Version 2.0")
+		                           .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+		                           .version("2.0")
+		                           .build();
 	}
 }
